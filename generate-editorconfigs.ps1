16a83c2bf4027343d05e4bae48571c99ed59d7f4
Gérald Barré <meziantou@users.noreply.github.com>
2023-12-19 19:38:09 +0000

    Update dependencies

--- generate-editorconfigs.ps1
+++ generate-editorconfigs.ps1
@@ -1,1 +1,5 @@
+$env:PATH="$HOME/.dotnet/tools:$env:PATH"
+& dotnet tool update --global --no-cache depsupdater
+& depsupdater update --directory $PSScriptRoot
+
 dotnet run --project $PSScriptRoot/tools/ConfigFilesGenerator/ConfigFilesGenerator.csproj
