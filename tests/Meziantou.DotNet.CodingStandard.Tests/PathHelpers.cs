1f7aeacf1d4e32134d8529904da779d7760e2863
Gérald Barré <meziantou@users.noreply.github.com>
2024-02-21 23:29:07 -0500

    Add automated tests (#83)

---
+++ tests/Meziantou.DotNet.CodingStandard.Tests/PathHelpers.cs
@@ -0,0 +1,17 @@
+﻿using Meziantou.Framework;
+
+namespace Meziantou.DotNet.CodingStandard.Tests;
+
+internal static class PathHelpers
+{
+    public  static FullPath GetRootDirectory()
+    {
+        var directory = FullPath.CurrentDirectory();
+        while (!Directory.Exists(directory / ".git"))
+        {
+            directory = directory.Parent;
+        }
+
+        return directory;
+    }
+}
---
+++ tests/Meziantou.DotNet.CodingStandard.Tests/SharedHttpClient.cs
@@ -0,0 +1,75 @@
+﻿#nullable enable
+namespace Meziantou.DotNet.CodingStandard.Tests;
+internal static class SharedHttpClient
+{
+    public static HttpClient Instance { get; } = CreateHttpClient();
+
+    private static HttpClient CreateHttpClient()
+    {
+        var socketHandler = new SocketsHttpHandler()
+        {
+            PooledConnectionIdleTimeout = TimeSpan.FromMinutes(1),
+            PooledConnectionLifetime = TimeSpan.FromMinutes(1),
+        };
+
+        return new HttpClient(new HttpRetryMessageHandler(socketHandler), disposeHandler: true);
+    }
+    private sealed class HttpRetryMessageHandler : DelegatingHandler
+    {
+        public HttpRetryMessageHandler(HttpMessageHandler handler)
+            : base(handler)
+        {
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            const int MaxRetries = 5;
+            var defaultDelay = TimeSpan.FromMilliseconds(200);
+            for (var i = 1; ; i++, defaultDelay *= 2)
+            {
+                TimeSpan? delayHint = null;
+                HttpResponseMessage? result = null;
+
+                try
+                {
+                    result = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+                    if (!IsLastAttempt(i) && ((int)result.StatusCode >= 500 || result.StatusCode is System.Net.HttpStatusCode.RequestTimeout or System.Net.HttpStatusCode.TooManyRequests))
+                    {
+                        // Use "Retry-After" value, if available. Typically, this is sent with
+                        // either a 503 (Service Unavailable) or 429 (Too Many Requests):
+                        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Retry-After
+
+                        delayHint = result.Headers.RetryAfter switch
+                        {
+                            { Date: { } date } => date - DateTimeOffset.UtcNow,
+                            { Delta: { } delta } => delta,
+                            _ => null,
+                        };
+
+                        result.Dispose();
+                    }
+                    else
+                    {
+                        return result;
+                    }
+                }
+                catch (HttpRequestException)
+                {
+                    result?.Dispose();
+                    if (IsLastAttempt(i))
+                        throw;
+                }
+                catch (TaskCanceledException ex) when (ex.CancellationToken != cancellationToken) // catch "The request was canceled due to the configured HttpClient.Timeout of 100 seconds elapsing"
+                {
+                    result?.Dispose();
+                    if (IsLastAttempt(i))
+                        throw;
+                }
+
+                await Task.Delay(delayHint is { } someDelay && someDelay > TimeSpan.Zero ? someDelay : defaultDelay, cancellationToken).ConfigureAwait(false);
+
+                static bool IsLastAttempt(int i) => i >= MaxRetries;
+            }
+        }
+    }
+}
